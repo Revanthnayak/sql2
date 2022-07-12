601fd8e90251a799bbd586526f0169538b326095
Revanthnayak <109086618+Revanthnayak@users.noreply.github.com>
2022-07-12 12:10:01 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,55 @@
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+
 from flask import Flask
-from flask import render_template
+from flask import redirect
+
+# Flask constructor takes the name of
+# current module (__name__) as argument.
+
 app = Flask(__name__)
+
+# Flask Routing
+
+# The route() function of the Flask class is a decorator,
+# which tells the application which URL should call
+# the associated function.
+
 @app.route('/')
-def index():
-	return render_template('index.html', pagetitle='Flask server home page')
-@app.route('/priya')
-def priya():
-	return render_template('priya.html', pagetitle='About Priya')
+# ‘/’ URL is bound with hello_world() function.
+def hello_world():
+	return 'rvce.edu.in'
+
+@app.route('/rv')
+def rvce():
+  return 'RVCE ROCKS'
+  app.add_url_rule('/', 'rvce', rvce)
+
+
+@app.route('/hello')
+def hello():
+	return redirect("http://www.rvce.edu.in", code=302)
+
+#Flask Variable rule
+
+@app.route('/hello/<name>')
+def hello_name(name):
+	return 'Hello %s!' % name
+
+@app.route('/rev/<float:revno>')
+def revision(revno):
+	return 'Revision number %f' % revno
+
+
+#Flask URL building
+
+
+
+
+
+# main driver function
 if __name__ == '__main__':
-	app.run(debug=True)
+
+	# run() method of Flask class runs the application
+	# on the local development server.
+	app.run()
